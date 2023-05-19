91618d2a00f179d942ba8846924400d7157af43d
jisu <onpaso53@gmail.com>
2023-05-19 23:08:11 +0900

    🔨 Refact: add different ways

--- 5.19jisukim/prime.py
+++ 5.19jisukim/prime.py
@@ -4,18 +4,55 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-nums = list(map(int,input().split()))
+nums = list(map(int, input().split()))
 # print(nums) -> [1, 3, 5, 7]
 # nums = map(int,input().split()) -> map object
 
-count = 0 
+count = 0
 for num in nums:
     error = 0
-    if num>1:
-        for x in range(2,num):
+    if num > 1:
+        for x in range(2, num):
             if num % x == 0:
                 error = 1
         if error == 0:
-            count+=1
-                
+            count += 1
+
+print(count)
+
+# # 에라토스테네스의 체
+
+
+# def prime_list(n):
+#     # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
+#     sieve = [True] * n
+#     sieve[0] = sieve[1] = False
+
+#     m = int(n ** 0.5)
+#     for i in range(2, m + 1):
+#         if sieve[i] == True:           # i가 소수인 경우
+#             for j in range(i*2, n, i):  # i이후 i의 배수들을 False 판정
+#                 sieve[j] = False
+
+#     # 소수 개수 산출
+#     print(sieve.count(True))
+
+
+# prime_list(10)
+
+# 에라토스테네스 체를 이용하여 백준 문제 풀이
+sieve = [True]*1000
+sieve[0] = sieve[1] = False
+for i in range(2, int(1000 ** 0.5) + 1):
+    for j in range(i * 2, 1000, i):
+        sieve[j] = False
+
+N = int(input())
+nums = map(int, input().split())
+
+count = 0
+
+for k in nums:
+    if sieve[k]:
+        count += 1
 print(count)
--- 5.19jisukim/star2.py
+++ 5.19jisukim/star2.py
@@ -4,14 +4,27 @@ input = sys.stdin.readline
 n = int(input())
 
 # sol1 그냥 풀어보기
-for i in range(1,n+1):
+for i in range(1, n+1):
     print(' '*(n-i)+'*'*i)
 
 # sol2 오른쪽 정렬
 for i in range(n):
     star = '*'*(i+1)
     print(star.rjust(n))
-    # 전체 n에서 오른쪽 정렬을 하는 .rjust(n) 사용
+# 전체 n에서 오른쪽 정렬을 하는 .rjust(n) 사용
 
-# sol3 재귀로 풀기
+# sol3 재귀 이용하기
 
+
+def solution(n, x):
+    N = x
+    if n <= 1:
+        return n
+    # 공백은, n이 4, 3, 2, 1, 0
+    # 별은, x가 1, 2, 3, 4, 5 이렇게 되면 된다.
+    print(' '*(N-n)+'*'*solution(n-1, N))
+    return n
+
+
+n = int(input())
+solution(n+1, n+1)
