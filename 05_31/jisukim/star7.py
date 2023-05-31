af923b8db0bf64cfcd6437725585d4f495249ff1
jisu <onpaso53@gmail.com>
2023-05-31 16:28:00 +0900

    :hammer:Refact:풀이 수정

--- 05_31/jisukim/star7.py
+++ 05_31/jisukim/star7.py
@@ -16,7 +16,7 @@ for i in range(N,0,-1):
 for i in range(1,N):
     print(' '*(N-i) + '*'*(2*i-1))
 
-for i in reversed(range(1,N)):
+for i in reversed(range(1,N+1)):
     print(' '*(N-i) + '*'*(2*i-1))
 
 
